d17f53fb55b5902ad8ed37e6b6ae313080675104
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-07-23 16:23:05 +0200

    basic of back-end plus jade render-engine

---
+++ app.js
@@ -0,0 +1,25 @@
+// Back-end stuff
+// Created by Fredrik A. Madsen-Malmo
+
+// Imports
+var express = require('express');
+
+// Create app
+var app = express();
+app.set('view engine', 'jade');
+
+// Routes
+app.get('/', function (req, res) {
+    res.render('index', { title: "" });
+});
+
+// Settings
+var PORT = process.env.PORT | 3000;
+
+// Server
+var server = app.listen(PORT, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Server is listening on http://%s:%s', host, port);
+});
