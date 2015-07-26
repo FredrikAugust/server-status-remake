c6837a8558973e3cac2f387d44a21e354df52679
Fredrik A. Madsen-Malmo <mail.fredrikaugust@gmail.com>
2015-07-26 18:00:53 +0200

    remembered to implement stuff

--- loop.js
+++ loop.js
@@ -17,13 +17,16 @@ var cputemp_str = "sensors | grep 'Physical id 0:'";
  */
 client.getcommand(cputemp_re, cputemp_str).then(function (result_get) {
 	db.insert('temp', result_get).then(function (result) {}, function (err) {});
-}, function (err_get) {});
+}, function (err_get) {
+	console.log(err_get);
+});
 
 /**
  * Gets the CPU Load frmo the client.getcommand function and inserts that
  * to the database
  */
 client.getcommand(cpuload_re, cpuload_str).then(function (result_get) {
-	// db.insert('load', result_get).then(function (result) {}, function (err) {});
-	console.log(result_get);
-}, function (err_get) {});
+	db.insert('load', result_get).then(function (result) {}, function (err) {});
+}, function (err_get) {
+	console.log(err_get);
+});
